15710de5449ec812d8ddc5d88a5e98ae383f25ef
Lukáš Červeňák <lukascervenak1109@gmail.com>
2026-01-20 12:36:16 +0100

    test

---
+++ hra.py
@@ -0,0 +1,17 @@
+import sys
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
