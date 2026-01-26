9c7ecb55c3165f0f9949319aaaa644ede50b7b11
Tomáš Ivan <tomas.ivan.it@gmail.com>
2026-01-26 08:59:44 +0100

    Pipes
    Používam jeden obrázok a pri hornej pipe si ho len otočím

--- hra.py
+++ hra.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import random
 import pygame
 
 pygame.init()
@@ -12,6 +13,7 @@ assets_dir = os.path.join(os.path.dirname(__file__), "assets")
 bg_path = os.path.join(assets_dir, "images", "background.png")
 floor_path = os.path.join(assets_dir, "images", "floor.png")
 bird_path = os.path.join(assets_dir, "images", "bird.png")
+pipe_path = os.path.join(assets_dir, "images", "pipe.png")
 
 background = pygame.image.load(bg_path).convert()
 background = pygame.transform.scale(background, screen.get_size())
@@ -20,6 +22,17 @@ floor_img = pygame.image.load(floor_path).convert_alpha()
 floor_height = floor_img.get_height()
 floor_img = pygame.transform.scale(floor_img, (screen.get_width(), floor_height))
 floor_y = screen.get_height() - floor_img.get_height()
+
+pipe_img = pygame.image.load(pipe_path).convert_alpha()
+pipe_top_img = pygame.transform.flip(pipe_img, False, True)
+pipe_w, pipe_h = pipe_img.get_size()
+
+PIPE_GAP = 160
+PIPE_SPEED = 3
+PIPE_SPAWN_MS = 1400
+
+pipes = []
+last_pipe_spawn_ms = pygame.time.get_ticks()
 
 bird_img = pygame.image.load(bird_path).convert_alpha()
 bird_w, bird_h = bird_img.get_size()
@@ -40,6 +53,19 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
+    now_ms = pygame.time.get_ticks()
+    if now_ms - last_pipe_spawn_ms >= PIPE_SPAWN_MS:
+        min_gap_y = 50
+        max_gap_y = floor_y - 50 - PIPE_GAP
+        if max_gap_y > min_gap_y:
+            gap_y = random.randint(int(min_gap_y), int(max_gap_y))
+            pipes.append({"x": screen.get_width() + 20, "gap_y": gap_y})
+        last_pipe_spawn_ms = now_ms
+
+    for pipe in pipes:
+        pipe["x"] -= PIPE_SPEED
+    pipes = [p for p in pipes if p["x"] + pipe_w > 0]
+
     bird_vel_y += gravity
     bird_y += bird_vel_y
     if bird_y + bird_h >= floor_y:
@@ -47,6 +73,11 @@ while running:
         bird_vel_y = 0.0
 
     screen.blit(background, (0, 0))
+    for pipe in pipes:
+        top_y = pipe["gap_y"] - pipe_h
+        bottom_y = pipe["gap_y"] + PIPE_GAP
+        screen.blit(pipe_top_img, (pipe["x"], top_y))
+        screen.blit(pipe_img, (pipe["x"], bottom_y))
     screen.blit(bird_img, (bird_x, bird_y))
     screen.blit(floor_img, (0, floor_y))
     pygame.display.flip()
